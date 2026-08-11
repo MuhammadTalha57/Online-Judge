baeab4d7abbaa1cc8a5648f6ce301f8f10e6d913
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-08-11 19:04:59 +0500

    Added migration for contests table

---
+++ db-service/src/migrations/1786456768460_create-contests-table.ts
@@ -0,0 +1,21 @@
+import type { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+    pgm.createTable('Contests', {
+        contest_id: 'id',
+        start_at: {
+            type: 'timestamp',
+            notNull: true,
+        },
+        duration: {
+            type: 'interval',
+            notNull: true
+        }
+    })
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+    pgm.dropTable('Contests')
+}
