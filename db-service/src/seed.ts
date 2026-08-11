157a4dc30e78802d0cd1cd56515f5e8aea7086ab
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-08-12 00:05:12 +0500

    Fixed bugs

--- db-service/src/seed.ts
+++ db-service/src/seed.ts
@@ -6,7 +6,7 @@ async function runSeed() {
     console.log(`Seeding DB`)
 
     try {
-        await queryMaster("INSERT INTO Users (username, password_hash, role) VALUES ($1, $2, 'admin')", [process.env.POSTGRESQL_WRITE_USER, await bcrypt.hash(process.env.POSTGRESQL_WRITE_USER_PASSWORD!, 10)])
+        await queryMaster(`INSERT INTO "Users" (username, password_hash, role) VALUES ($1, $2, 'admin')`, [process.env.POSTGRESQL_WRITE_USER, await bcrypt.hash(process.env.POSTGRESQL_WRITE_USER_PASSWORD!, 10)])
         console.log("Database seeded successfully")
     } catch(error) {
         console.error(`Seeding Failed: ${error}`)
