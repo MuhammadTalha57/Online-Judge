19b80629cbaae6eee33e0b6e0e47ede397134cc6
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-08-11 21:34:16 +0500

    Defined db seed

---
+++ db-service/src/seed.ts
@@ -0,0 +1,17 @@
+import bcrypt from "bcryptjs"
+import { queryMaster } from "./db.js"
+import 'dotenv/config'
+
+async function runSeed() {
+    console.log(`Seeding DB`)
+
+    try {
+        await queryMaster("INSERT INTO Users (username, password_hash, role) VALUES ($1, $2, 'admin')", [process.env.POSTGRESQL_WRITE_USER, await bcrypt.hash(process.env.POSTGRESQL_WRITE_USER_PASSWORD!, 10)])
+        console.log("Database seeded successfully")
+    } catch(error) {
+        console.error(`Seeding Failed: ${error}`)
+        process.exit(1);
+    }
+}
+
+runSeed()
