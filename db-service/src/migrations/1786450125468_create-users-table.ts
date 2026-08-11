89ec9b042247ff384920656988a3a0659c8a9011
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-08-11 17:27:19 +0500

    created migration for users table

---
+++ db-service/src/migrations/1786450125468_create-users-table.ts
@@ -0,0 +1,30 @@
+import type { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+	pgm.createTable("Users", {
+		user_id: {
+			type: "uuid",
+			primaryKey: true,
+			default: pgm.func("gen_random_uuid()"),
+		},
+		username: {
+			type: "varchar(255)",
+			notNull: true,
+		},
+		password_hash: {
+			type: "varchar(255)",
+			notNull: true,
+		},
+		role: {
+			type: "varchar(5)",
+			notNull: true,
+			default: "user",
+		},
+	});
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+	pgm.dropTable("users");
+}
